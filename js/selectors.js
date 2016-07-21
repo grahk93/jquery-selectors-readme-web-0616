1a27618271b5a90c8bb4beb8477f1054dadbcddd
grahk93 <grace.juster@flatironschool.com>
2016-07-21 18:38:21 -0400

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,24 @@
-// declare your functions here...
+function paragraphSelector() {
+  paragraphs = $('p')
+  return paragraphs
+}
+
+function lastImageSelector() {
+  lastImage = $('img:last')
+  return lastImage
+}
+
+function ninjaBabySelector() {
+  baby = $('#baby-ninja')
+  return baby
+}
+
+function divSelector() {
+  divs = $('div')
+  return divs
+}
+
+function firstListItem() {
+  item = $('#pic-list li:first-child')
+  return item
+}
